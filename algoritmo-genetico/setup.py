89f406e871fedebc049598bd78888a33146ccbb1
Jorge Guilherme <jorgekg3@gmail.com>
2019-05-05 16:26:59 -0300

    adicionado plot

--- algoritmo-genetico/setup.py
+++ algoritmo-genetico/setup.py
@@ -1,6 +1,8 @@
 from cities import Cities
 from population import Population
 from distance import DistanceResolver
+import matplotlib.pyplot as plt
+import matplotlib
 
 cities = Cities()
 population = Population(cities)
@@ -12,3 +14,10 @@ for _ in range(10000):
 for chromosome in population.get_population():
     print(chromosome.get_cities())
     print(DistanceResolver(cities).calculate_distance(chromosome))
+
+cmap = matplotlib.cm.flag
+cmap.set_bad(color='white')
+plt.imshow([[1,1,1], [1,1,1], [1,1,1]], cmap)
+plt.show(block=True)
+plt.pause(0.1)
+plt.clf()
