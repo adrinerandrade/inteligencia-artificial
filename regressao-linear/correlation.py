6ad1ce0121bf71c793a857cb6eddf5d2323beacc
Adriner Andrade <adriner_andrade@hotmail.com>
2019-06-03 20:32:12 -0300

    questão '

--- regressao-linear/correlation.py
+++ regressao-linear/correlation.py
@@ -1,16 +1,23 @@
-import operator
-from functools import reduce
-
 class Correlation:
+  def __init__(self, data):
+    self.x = data[0]
+    self.y = data[1]
+    self.mediaX = self.media(self.x)
+    self.mediaY = self.media(self.y)
 
-  def __init__(self, x_array, y_array):
-    self.x_array = x_array
-    self.y_array = y_array
+  def calc(self):
+    # sem criatividade para nome de variaveis
+    val1 = 0
+    val2 = 0
+    val3 = 0
+    for i in range(len(self.x)):
+      val1 = val1 + ((self.x[i] - self.mediaX) * (self.y[i] - self.mediaY))
+      val2 = val2 + ((self.x[i] - self.mediaX) ** 2)
+      val3 = val3 + ((self.y[i] - self.mediaY) ** 2)
+    return (val1 / ((val2 * val3) ** (1/2)))
 
-  def calc_r(self):
-    x_average = self.average(self.x_array)
-    y_average = self.average(self.y_array)   
-
-  def average(self, arr):
-    sum = reduce(operator.add, arr)
-    return sum / len(arr)
+  def media(self, list):
+    total_value = 0
+    for value in list:
+      total_value = total_value + value
+    return total_value / len(list)
