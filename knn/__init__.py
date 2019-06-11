0b689326015f04463b8b1e0f9e9b937cfbfd7ebc
Adriner Andrade <adriner_andrade@hotmail.com>
2019-06-11 19:37:09 -0300

    implementando a primeira questão

--- knn/__init__.py
+++ knn/__init__.py
@@ -5,8 +5,16 @@ from knn import doKnn
 mat = scipy.loadmat('./data/grupoDados1.mat')
 grupoTrain = mat['grupoTrain']
 grupoTest = mat['grupoTest']
-testRotulos = mat['testRots']
 trainRotulos = mat['trainRots']
+testRotulos = mat['testRots']
 
 rotulos_previstos = doKnn(grupoTrain, trainRotulos, grupoTest, 1)
+total_test = len(testRotulos)
+total_corretos = 0
+for i in range(total_test):
+  train_rotulo = testRotulos[i][0]
+  if train_rotulo == rotulos_previstos[i]:
+    total_corretos += 1
+
+print("Acurácia: {}", total_corretos / total_test)
 print(rotulos_previstos)
--- knn/knn.py
+++ knn/knn.py
@@ -12,10 +12,10 @@ def doKnn(dadosTrain, rotuloTrain, dadosTeste, k):
   rotulos_previstos = [0 for x in range(len(dadosTeste))]
   for i in range(len(matriz_distancia)):
     distanciaOrdenada = sorted(matriz_distancia[i], key=lambda distancia: distancia[0])
-    rotulos_ordenados = map(lambda x: x[0], distanciaOrdenada)
-    rotulos_ordenados
-    # np.bincount([1, 2, 3, 1, 2, 1, 1, 1, 3, 2, 2, 1]).argmax()
-    rotulos_previstos[i] = matriz_distancia[i]
+    rotulos_ordenados = list(map(lambda x: x[1], distanciaOrdenada))
+    # calculando a moda para obter a classificação
+    classificacao = numpy.argmax(numpy.bincount(rotulos_ordenados[0:k]))
+    rotulos_previstos[i] = classificacao
 
   return rotulos_previstos
 
@@ -24,5 +24,5 @@ def calc_distancia(elem_teste, vetor_train, rotulos_train):
   for i in range(len(vetor_train)):
     distancia_elemento = numpy.subtract(elem_teste, vetor_train[i])
     distancia_calc = math.sqrt(numpy.sum(numpy.power(distancia_elemento, 2)))
-    distancia[i] = (distancia_calc, rotulos_train[i])
+    distancia[i] = (distancia_calc, rotulos_train[i][0])
   return distancia
